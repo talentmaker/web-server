f21bc39846d82f57357ef540284fd8e2b6c31a60
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2021-12-26 04:25:43 +0000

    feat: serve static files

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Static("/", "static/site")
+
+	app.Get("*", func(c *fiber.Ctx) error {
+		return c.SendFile("static/site/index.html")
+	})
+
+	app.Listen(":3000")
+}
