3d7bd8f55c32ada50a1e35bf53f9a2fcda4eadfb
Luke-zhang-04 <Luke_zhang_04@protonmail.com>
2021-12-28 18:14:04 +0000

    feat(site): add helmet, limiter, and compression

--- site/go.mod
+++ site/go.mod
@@ -4,12 +4,12 @@ go 1.17
 
 require (
 	github.com/gofiber/fiber/v2 v2.23.0
+	github.com/gofiber/helmet/v2 v2.2.5
 	github.com/talentmaker/rendertronmiddleware v0.0.0-20211228170137-52fa44d5f8a2
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/gofiber/helmet/v2 v2.2.5 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.31.0 // indirect
--- site/main.go
+++ site/main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"time"
+
 	rendertron "github.com/talentmaker/rendertronmiddleware"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/helmet/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Use(helmet.New())
+	app.Use(
+		helmet.New(),
+		limiter.New(limiter.Config{
+			Max:        100,
+			Expiration: 1 * time.Minute,
+			KeyGenerator: func(ctx *fiber.Ctx) string {
+				return ctx.IP()
+			},
+			LimitReached: func(ctx *fiber.Ctx) error {
+				return ctx.Status(429).SendString("Too many requests")
+			},
+		}),
+		compress.New(compress.Config{
+			Level: compress.LevelBestSpeed,
+		}),
+	)
 
 	app.Use(rendertron.New(rendertron.Options{
 		ProxyUrl: "http://localhost:8000/render",
@@ -23,7 +42,9 @@ func main() {
 		},
 	}))
 
-	app.Static("/", "./static/site")
+	app.Static("/", "./static/site", fiber.Static{
+		Compress: true,
+	})
 
 	app.Get("*", func(ctx *fiber.Ctx) error {
 		return ctx.SendFile("./static/site/index.html")
